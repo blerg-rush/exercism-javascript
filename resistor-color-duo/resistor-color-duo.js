1d36928aec50a18cf2d5eea957beef9335f51b60
TheWillGabriel <will.gabriel@gmail.com>
2019-10-20 13:06:20 -0600

    Destructure value array parameters

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,5 +1,5 @@
-export const value = (colorArray) =>
-  parseInt(`${COLORS.indexOf(colorArray[0])}${COLORS.indexOf(colorArray[1])}`)
+export const value = ([firstColor, secondColor]) =>
+  parseInt(`${COLORS.indexOf(firstColor)}${COLORS.indexOf(secondColor)}`)
 
 export const COLORS = [
   'black', 'brown', 'red', 'orange', 'yellow',

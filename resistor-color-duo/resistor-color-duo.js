e312094ef9ce7d1d1c285c0df810e964d13262d8
TheWillGabriel <will.gabriel@gmail.com>
2019-10-20 13:08:47 -0600

    Use multiplication to avoid conversion of index to string

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,5 +1,5 @@
 export const value = ([firstColor, secondColor]) =>
-  parseInt(`${COLORS.indexOf(firstColor)}${COLORS.indexOf(secondColor)}`)
+  10 * COLORS.indexOf(firstColor) + COLORS.indexOf(secondColor)
 
 export const COLORS = [
   'black', 'brown', 'red', 'orange', 'yellow',

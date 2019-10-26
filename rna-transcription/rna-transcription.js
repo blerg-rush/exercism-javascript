cb363b4c316b6db39e1e0dea6fe4e01d718596f6
TheWillGabriel <will.gabriel@gmail.com>
2019-10-26 16:39:52 -0600

    Switch to assignment operator instead of concat

---
+++ rna-transcription/rna-transcription.js
@@ -0,0 +1,12 @@
+const dnaComplements = { 'G': 'C',
+                         'C': 'G',
+                         'T': 'A',
+                         'A': 'U'}
+
+export const toRna = (sequence) => {
+  let rna = ''
+  for (let nucleotide of sequence) {
+    rna += dnaComplements[nucleotide]
+  }
+  return rna
+}

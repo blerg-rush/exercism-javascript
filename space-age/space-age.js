2a46d010b75dc58e77925a72b612b020d73ef320
TheWillGabriel <will.gabriel@gmail.com>
2019-11-01 09:32:34 -0600

    Pass first test

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,4 +1,5 @@
-const RELATIVE_ORBITAL_PERIODS = { "mercury": 0.2408467,
+const RELATIVE_ORBITAL_PERIODS = { "earth": 1.0,
+                                   "mercury": 0.2408467,
                                    "venus": 0.61519726,
                                    "mars": 1.8808158,
                                    "jupiter": 11.862615,
@@ -6,6 +7,10 @@ const RELATIVE_ORBITAL_PERIODS = { "mercury": 0.2408467,
                                    "uranus": 84.016846,
                                    "neptune": 164.79132 }
 
-export const age = () => {
+const timeInEarthYears = (seconds) => {
+  return seconds / 31557600.0
+}
 
+export const age = (planet, seconds) => {
+  return +(timeInEarthYears(seconds) * RELATIVE_ORBITAL_PERIODS[planet]).toFixed(2)
 };

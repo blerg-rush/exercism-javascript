51872d05cf1c19aa3d03f3afe23a88883c143951
TheWillGabriel <will.gabriel@gmail.com>
2019-11-01 09:37:42 -0600

    Clean up code formatting

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,16 +1,19 @@
-const RELATIVE_ORBITAL_PERIODS = { "earth": 1.0,
-                                   "mercury": 0.2408467,
-                                   "venus": 0.61519726,
-                                   "mars": 1.8808158,
-                                   "jupiter": 11.862615,
-                                   "saturn": 29.447498,
-                                   "uranus": 84.016846,
-                                   "neptune": 164.79132 }
+const RELATIVE_ORBITAL_PERIODS = {
+  earth: 1.0,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+}
 
 const timeInEarthYears = (seconds) => {
   return seconds / 31557600.0
 }
 
 export const age = (planet, seconds) => {
-  return +(timeInEarthYears(seconds) / RELATIVE_ORBITAL_PERIODS[planet]).toFixed(2)
-};
+  return +(timeInEarthYears(seconds) / RELATIVE_ORBITAL_PERIODS[planet])
+    .toFixed(2)
+}

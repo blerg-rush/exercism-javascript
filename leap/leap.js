03eb880327e950adf3956418125c44bfdf182b91
TheWillGabriel <will.gabriel@gmail.com>
2019-11-02 18:13:14 -0600

    Simplify conditional statement to one line

--- leap/leap.js
+++ leap/leap.js
@@ -1,10 +1,3 @@
 export const isLeap = (year) => {
-  if(year % 400 == 0){
-    return true
-  } else if(year % 100 == 0){
-    return false
-  } else if(year % 4 == 0){
-    return true
-  }
-  return false
-};
+  return (year % 400 == 0) || (!(year % 100 == 0) && (year % 4 == 0))
+}
